2c2ed924de434dabbb035a7be13871f3c14375ce
Arifur Rahman <arifr956@gmail.com>
2023-10-20 01:02:19 +0600

    Removing node_modules

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -20,7 +21,8 @@ app.get('/',(req,res)=> {
 
 
 
-const uri = "mongodb+srv://arifr956:BO4WrpJYliABwW3L@cluster0.moucvko.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.moucvko.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
